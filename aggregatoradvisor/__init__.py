145e5e471a61469d8d7b24844ed574c6957dfce7
Teague Sterling (UCSF) <teague@band.desktop.ucsf.bkslab.org>
2014-10-21 12:59:11 -0700

    Renaming

--- aggregatoradvisor/__init__.py
+++ aggregatoradvisor/__init__.py
@@ -6,7 +6,7 @@ from flask.ext.login import LoginManager
 from admin_auth import AuthenticatedAdminIndexView
 
 app = Flask(__name__)
-app.config.from_object('aggregatoradvisor.app.config')
+app.config.from_object('aggregatoradvisor.config')
 app.config.from_pyfile('aggregatoradvisor.cfg', silent=True)
 app.config.from_envvar('AGGREGATORADVISOR_CONFIG')
 db = SQLAlchemy(app)
