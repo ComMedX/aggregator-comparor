70dcf91f585f288a67c9994097eeaef5971a6355
Teague Sterling <teaguesterling@users.noreply.github.com>
2014-10-23 10:43:51 -0700

    Create core.py

---
+++ aggregatoradvisor/core.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.admin import Admin
+from flask.ext.login import LoginManager
+import admin_auth
+
+app = Flask(__name__)
+app.config.from_object('aggregatoradvisor.config')
+app.config.from_pyfile('aggregatoradvisor.cfg', silent=True)
+app.config.from_envvar('AGGREGATORADVISOR_CONFIG', silent=True)
+
+db = SQLAlchemy(app)
+login = LoginManager(app)
+login.user_loader(admin_auth.get_administrator)
+admin = Admin(app, index_view=admin_auth.AuthenticatedAdminIndexView())
+
