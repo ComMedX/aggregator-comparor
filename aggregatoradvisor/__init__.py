3c023c61ab04853014cc9276c159740e4c45cd09
Teague Sterling <teaguesterling@users.noreply.github.com>
2014-10-23 10:44:47 -0700

    Update __init__.py

--- aggregatoradvisor/__init__.py
+++ aggregatoradvisor/__init__.py
@@ -1,20 +1,9 @@
-from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.admin import Admin
-from flask.ext.login import LoginManager
-
-import admin_auth
-
-app = Flask(__name__)
-app.config.from_object('aggregatoradvisor.config')
-app.config.from_pyfile('aggregatoradvisor.cfg', silent=True)
-app.config.from_envvar('AGGREGATORADVISOR_CONFIG', silent=True)
-db = SQLAlchemy(app)
-login = LoginManager(app)
-login.user_loader(admin_auth.get_administrator)
-admin = Admin(app, index_view=admin_auth.AuthenticatedAdminIndexView())
-
+from core import (
+  app,
+  db,
+  login,
+  admin,
+)
 import models
 import admin_ui
 import views
-
