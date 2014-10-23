f0c235e97e2cff9d85cebea5a9096377a2922c4f
Teague Sterling <teaguesterling@users.noreply.github.com>
2014-10-23 10:42:18 -0700

    Update config.py

--- aggregatoradvisor/config.py
+++ aggregatoradvisor/config.py
@@ -1,18 +1,18 @@
+PORT=8080
+DEBUG=True
+
 # Database Configuration
 SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://agad:agad@localhost/agad'
-SQLALCHEMY_ECHO = True
+SQLALCHEMY_ECHO = DEBUG
 
-# Administrator Configuration
+# Administrator Configuration { Username: (PASSWORD_HASH, EMAIL_ADDRESS), }
 ADMINS = {
-    # Username:  (PASSWORD_HASH, EMAIL_ADDRESS),
     'agadmin': ('frodo', 'email@address.com'),
 }
-
 
 # Forms Configuration
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'Shhh! This is a super secret key'
-
 
 # Display Configuration
 AGGREGATORS_DISPLAY_IMAGE_SIZE = (300,300)
